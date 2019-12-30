c10a0ba433db37bc2d66246d860a529bc6ee1a43
clueminds2510 <neha2510@gmail.com>
2019-12-30 19:21:21 +0530

    Second commit

--- src/main/java/com/skills/Main.java
+++ src/main/java/com/skills/Main.java
@@ -2,6 +2,7 @@ package com.skills;
 
 import java.io.IOException;
 
+
 import com.google.gson.JsonSyntaxException;
 import com.skills.process.Reader;
 import com.skills.process.SkillFinder;
@@ -9,7 +10,8 @@ import com.skills.process.SkillFinder;
 public class Main {
 	public static void main(String[] args) throws JsonSyntaxException, IOException {
 		System.out.println("Skill Finder....");
-		String skill=new Reader().Read();
+		Reader reader = new Reader();
+		String skill=reader.Read();
 		SkillFinder sf = new SkillFinder();
 		sf.finder(skill);
 	}
